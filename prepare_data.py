31ce8244047b7a4e360ec1b1575bf61445c1724b
ziqiaozhou <ziqiao@cs.unc.edu>
2019-12-29 05:12:08 -0500

    prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -2,56 +2,57 @@ import numpy as np
 from sklearn.feature_selection import RFE, VarianceThreshold,SelectKBest, chi2
 from sklearn.svm import SVR
 from IPython import embed
-
+import pandas as pd
+import datetime
+def getSymVar(columns):
+    symbol_vars={"c":[],"I":[],"Ialt":[],"s":[],"salt":[]}
+    for offset,sym_name in enumerate(columns):
+        all=sym_name.split("_")
+        label=all[0]
+        index=all[1]
+        symbol_vars[label].append(offset)
+    return symbol_vars
 def prepare_data(samedata_file,diffdata_file):
-    if diffdata_file:
-        same_data=np.genfromtxt(samedata_file,filling_values=2,delimiter=",")
-        diff_data=np.genfromtxt(diffdata_file,filling_values=2,delimiter=",")
-        x=np.concatenate((same_data,diff_data))
-        y=np.concatenate((np.zeros((same_data.shape[0],1)),np.ones((diff_data.shape[0],1))))
-        now = datetime.now()
-        np.savez(now.strftime("%m_%D_%H_%M_%S"),X,Y)
-    else:
-        data=np.load(samedata_file)
-        x=data["arr_0"]
-        y=data["arr_1"]
-    d=x.shape[-1]
-    split_index=np.unique(np.where(x==2)[1])
+    if diffdata_file==None:
+        data=pd.read_csv(samedata_file)
+        embed()
+        return (data[data.columns[2:]].to_numpy(),data["Y"].to_numpy(),data.columns[2:],getSymVar(data.columns[2:]))
+    same_data=pd.read_csv(samedata_file,header=None)
+    same_data.insert(0, 'Y', [0]*len(same_data))
+    diff_data=pd.read_csv(diffdata_file,header=None)
+    diff_data.insert(0, 'Y', [1]*len(diff_data))
+    #same_data=np.genfromtxt(samedata_file,filling_values=2,delimiter=",")
+    #diff_data=np.genfromtxt(diffdata_file,filling_values=2,delimiter=",")
+    data=pd.concat([same_data,diff_data])
+    onerow=data[0:1].to_numpy();
+    d=onerow.shape[-1]
+    split_index=np.unique(np.where(onerow==' ')[1])
     assert(split_index.shape[0]>2)
-    cs=range(0,split_index[0])
-    I=range((split_index[0]+1),split_index[1])
-    IAlt=range((split_index[1]+1),split_index[2])
-    s=range((split_index[2]+1),split_index[3])
-    sAlt=range((split_index[3]+1),split_index[4])
-    symbols={"s":s,"salt":sAlt,"c":cs,"I":I,"Ialt":IAlt}
-    M={}
-    count={"s":0,"salt":0,"c":0,"I":0,"Ialt":0,"none":0}
-    for label in symbols:
-        var=symbols[label]
-        for i in var:
-            name="%s_%d"%(label,count[label])
-            M[i]=name
-            count[label]=count[label]+1
-    cols=[]
-    vtype={}
-    symbol_vars={"s":[],"salt":[],"c":[],"I":[],"Ialt":[]}
+    widths=split_index[0:5]-np.concatenate((np.array([0]),split_index[:-1]))[0:5]
+    widths=widths-1
+    cols=["Y"]
+    colnames=["c","I","Ialt","s","salt"]
+    for i,w in enumerate(widths):
+        cols=cols+["%s_%d"%(colnames[i],j) for j in range(w)]
+        cols.append("none")
+    while len(cols)<d:
+        cols.append("none")
+    data.columns=cols
+    del data["none"]
     select=VarianceThreshold(0.00001)
     #np.random.shuffle(x)
     #np.random.shuffle(y)
-    estimator = SVR(kernel="linear")
-    selector = RFE(estimator, 5, step=1)
-    newX=select.fit_transform(x, y)
+    select.fit(data, data["Y"])
     #select=RFE(chi2, k=20)
     index=np.where(select.get_support())[0]
-    for offset in range(index.shape[0]):
-        i=index[offset]
-        if i not in M:
-            label="none"
-            name=label
-        else:
-            name=M[i]
-            label=name.split("_")[0]
-            symbol_vars[label].append(offset)
-        cols.append(name)
+    now = datetime.datetime.now()
+    index=list(set(index)-set([0]))
+    x=data.ix[:,index]
+    to_del_index=list(set(range(data.shape[-1]))-set([0])-set(index))
+    embed()
+    data.drop(data.columns[to_del_index],axis=1,inplace=True)
+    #del data.ix[:,to_del_index]
 
-    return (x[:,index],y,cols,symbol_vars)
+    embed()
+    data.to_csv(now.strftime("%Y_%m_%d_%H_%M_%S.csv"))
+    return (x.to_numpy(),data["Y"].to_numpy(),x.columns,getSymVar(x.columns))
