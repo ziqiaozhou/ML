d6b9c1c6846fc3b78f7a06d327bb6a19f2d09761
ziqiaozhou <ziqiao@cs.unc.edu>
2020-11-28 20:47:24 -0500

    remove useless rules

--- xgboost/scripts/indacc.sh
+++ xgboost/scripts/indacc.sh
@@ -1,3 +1,4 @@
 prefix=$1
 datafile=`ls $prefix*.csv`
-python3 rules2dag.py  --data=$datafile --sort=$2 --mode=indacc $prefix.rule.txt 
+python3 rules2dag.py  --data=$datafile --sort=$2 --mode=indacc\
+ --dev_ratio=$3 $prefix.rule.txt 
